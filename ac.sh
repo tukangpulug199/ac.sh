3051787633512eb15a5962908c4c223fbb5b4aba
ACILOADING <52864713+tukangpulug199@users.noreply.github.com>
2019-09-16 14:28:54 +0000

    Create ac.sh

---
+++ ac.sh
@@ -0,0 +1,43 @@
+!#usr/bin/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+clear
+figlet HACK FB
+sleep 3
+echo $pu"Author :"$i" Acil Sean"
+sleep 2
+echo $pu"Youtube :"$i"MR 4444"
+sleep 2
+echo $me"Warning : Gunakan Scrip Ini Dengan Bijak"
+sleep 2
+echo $cy"Version 1.1"
+sleep 1
+echo $pu"#=====================================================#"
+sleep 2
+echo""
+echo $i"["$ku"1"$i"]"$cy" Hack Facebook Target"
+sleep 3
+echo""
+echo $me
+read -p "Pilihan Anda>># " pil;
+sleep 4
+echo $me
+clear
+read -p "Masukan ID Target # " pil;
+sleep 7
+echo $ku"Mendapatkan Password Target"
+sleep 5
+echo $pu"Succes Mengambil Data Korban"
+sleep 2
+echo "Password :"$me"thejack1928"
+sleep 4
+echo $pu"Email :"$me"Not Found"
+sleep 3
+echo $ku"SUBSCRIBE MR 4444"
